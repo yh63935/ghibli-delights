d15a0497589dab12892a881ea1a48e90146f3a06
Amelia Ho <120755144+yh63935@users.noreply.github.com>
2024-01-19 13:49:06 -0800

    Add class option for creating an element

---
+++ src/navBar.js
@@ -0,0 +1,19 @@
+import {createEl} from './components.js';
+
+function createNavBar() {
+    const body = document.querySelector('body');
+    const header = document.createElement('header');
+    const navBar = document.createElement('nav');
+    const navList = document.createElement('ul');
+    const home = createEl('li', 'Home', 'home');
+    const menu = createEl('li', 'Menu', 'menu');
+    const contact = createEl('li', 'Contact', 'contact');
+    body.append(header);
+    header.append(navBar);
+    navBar.append(navList);
+    navList.append(home);
+    navList.append(menu);
+    navList.append(contact);
+}
+
+export default createNavBar;
