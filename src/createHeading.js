1285f76407d52f5f3bed3696bc2e5749a5d7c9f4
Amelia Ho <120755144+yh63935@users.noreply.github.com>
2024-01-19 22:49:18 -0800

    Delete createHeading.js file The function is now in components file

--- src/createHeading.js
+++
@@ -1,8 +0,0 @@
-function createHeading(heading) {
-    const h1 = document.createElement('h1');
-    h1.innerText = heading;
-    const body = document.querySelector('body');
-    body.append(h1);
-}
-
-export default createHeading;
